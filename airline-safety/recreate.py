f24462aa8ed1f43e67889ddb98862ec2d2920220
Andrew Jaeger <mrjaeger00@gmail.com>
2016-06-19 00:08:01 -0700

    Recreate airline-safety safety score table

--- airline-safety/recreate.py
+++ airline-safety/recreate.py
@@ -14,7 +14,8 @@ def recreate():
     # All data in the article is per 1 trillion seat kilometers per year
     # 52 weeks in a year, each period had 14.5 years in it
     # (I would think 15, but using 14.5 makes the numbers map to the first chart of the article)
-    scaling_factor = 1e12 / (airline_data['avail_seat_km_per_week'] * 52 * 14.5)
+    seat_kms_flown = (airline_data['avail_seat_km_per_week'] * 52 * 14.5)
+    scaling_factor = 1e12 / seat_kms_flown
 
     # Fatalities by Airline Are Highly Unpredictable graph
     airline_data['fatalities_85_99_per_1_trillion_sk'] = airline_data['fatalities_85_99'] * scaling_factor
@@ -34,7 +35,7 @@ def recreate():
     lin_plot.axes.set_xlim(-50,)
     lin_plot.axes.set_ylabel('2000-14')
     lin_plot.axes.set_ylim(-50,)
-    sns.plt.show()
+    # sns.plt.show()
 
     # Incidents by Airline Are Slightly Predictable graph
     airline_data['incidents_85_99_per_1_trillion_sk'] = airline_data['incidents_85_99'] * scaling_factor
@@ -54,7 +55,106 @@ def recreate():
     lin_plot.axes.set_xlim(-5,)
     lin_plot.axes.set_ylabel('2000-14')
     lin_plot.axes.set_ylim(-5,)
-    sns.plt.show()
+    # sns.plt.show()
+
+    # Calculate safety score as defined in article
+    airline_data['fatal_accidents_85_99_per_1_trillion_sk'] = (
+        airline_data['fatal_accidents_85_99'] * scaling_factor
+    )
+    airline_data['fatal_accidents_00_14_per_1_trillion_sk'] = (
+        airline_data['fatal_accidents_00_14'] * scaling_factor
+    )
+
+    average_fatality_rate_85_99 = np.average(
+        airline_data[['fatalities_85_99']].sum(axis=1)
+    )
+    average_fatal_accident_rate_85_99 = np.average(
+        airline_data[['fatal_accidents_85_99']].sum(axis=1)
+    )
+    average_incident_rate_85_99 = np.average(
+        airline_data[['incidents_85_99']].sum(axis=1)
+    )
+    average_fatality_rate_00_14 = np.average(
+        airline_data[['fatalities_00_14']].sum(axis=1)
+    )
+    average_fatal_accident_rate_00_14 = np.average(
+        airline_data[['fatal_accidents_00_14']].sum(axis=1)
+    )
+    average_incident_rate_00_14 = np.average(
+        airline_data[['incidents_00_14']].sum(axis=1)
+    )
+
+    # sample_size_factor = (seat_kms_flown / 2).pow(.5)
+    sample_size_factor = airline_data['avail_seat_km_per_week'].pow(.5)
+    airline_data['fatalities_component_85_99'] = (
+        stats.zscore(
+            (
+                average_fatality_rate_85_99 -
+                airline_data['fatalities_85_99_per_1_trillion_sk']
+            ) * sample_size_factor
+        )
+    )
+    airline_data['fatal_accidents_component_85_99'] = (
+        stats.zscore(
+            (
+                average_fatal_accident_rate_85_99 -
+                airline_data['fatal_accidents_85_99_per_1_trillion_sk']
+            ) * sample_size_factor
+        )
+    )
+    airline_data['incidents_component_85_99'] = (
+        stats.zscore(
+            (
+                average_incident_rate_85_99 -
+                airline_data['incidents_85_99_per_1_trillion_sk']
+            ) * sample_size_factor
+        )
+    )
+    airline_data['safety_score_85_99'] = (
+        airline_data[[
+            'fatalities_component_85_99',
+            'fatal_accidents_component_85_99',
+            'incidents_component_85_99'
+        ]].mean(axis=1)
+    )
+
+    airline_data['fatalities_component_00_14'] = (
+        stats.zscore(
+            (
+                average_fatality_rate_00_14 -
+                airline_data['fatalities_00_14_per_1_trillion_sk']
+            ) * sample_size_factor
+        )
+    )
+    airline_data['fatal_accidents_component_00_14'] = (
+        stats.zscore(
+            (
+                average_fatal_accident_rate_00_14 -
+                airline_data['fatal_accidents_00_14_per_1_trillion_sk']
+            ) * sample_size_factor
+        )
+    )
+    airline_data['incidents_component_00_14'] = (
+        stats.zscore(
+            (
+                average_incident_rate_00_14 -
+                airline_data['incidents_00_14_per_1_trillion_sk']
+            ) * sample_size_factor
+        )
+    )
+    airline_data['safety_score_00_14'] = (
+        airline_data[[
+            'fatalities_component_00_14',
+            'fatal_accidents_component_00_14',
+            'incidents_component_00_14'
+        ]].mean(axis=1)
+    )
+
+    # The exact scores computed do not exactly match the scores in the table provided in the article.
+    # The spirit of the orderings is correct, and withouth knowing the *exact* underlying methodology
+    # it would be very difficult to exactly rebuild the table.
+    airline_data['safety_score'] = airline_data[['safety_score_85_99', 'safety_score_00_14']].mean(axis=1)
+    print airline_data.sort(['safety_score'], ascending=0)[['airline', 'safety_score']]
 
 if __name__ == '__main__':
     recreate()
